108a706011955df7d2acdbab7796c22816c20507
Roger King <rogr.king@gmail.com>
2017-04-15 22:27:02 -0400

    feat(user_room):migration for user_room

--- api/priv/repo/migrations/20170416004328_create_user_room.exs
+++ api/priv/repo/migrations/20170416004328_create_user_room.exs
@@ -10,6 +10,7 @@ defmodule Huddle.Repo.Migrations.CreateUserRoom do
     end
     create index(:user_rooms, [:user_id])
     create index(:user_rooms, [:room_id])
+    # Prevent a user to join the same room twice.
     create index(:user_rooms, [:user_id, :room_id], unique: true)
   end
 end
