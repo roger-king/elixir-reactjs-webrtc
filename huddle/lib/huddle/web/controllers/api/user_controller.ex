1dc91f909e32c06d4964838fd731e2344fa94b78
roger-king <rogr.king@gmail.com>
2017-04-25 17:56:30 -0400

    enhancement: phoenix 1.3 project structure

---
+++ huddle/lib/huddle/web/controllers/api/user_controller.ex
@@ -0,0 +1,32 @@
+defmodule Huddle.Web.UserController do
+  use Huddle.Web, :controller
+
+    alias Huddle.User
+
+    plug Guardian.Plug.EnsureAuthenticated, [handler: Huddle.SessionController] when action in [:rooms]
+
+    def rooms(conn, _params)do
+      current_user = Guardian.Plug.current_resource(conn)
+      rooms = Repo.all(assoc(current_user, :rooms))
+      render(conn, Huddle.RoomView, "index.json", %{rooms: rooms})
+    end
+
+    def create(conn, params) do
+      IO.puts "Creating Users"
+      changeset = User.registration_changeset(%User{}, params)
+
+      case Repo.insert(changeset) do
+        {:ok, user} ->
+          new_conn = Guardian.Plug.api_sign_in(conn, user, :access)
+          jwt = Guardian.Plug.current_token(new_conn)
+
+          new_conn
+          |> put_status(:created)
+          |> render(Huddle.SessionView, "show.json", user: user, jwt: jwt)
+        {:error, changeset} ->
+          conn
+          |> put_status(:unprocessable_entity)
+          |> render(Huddle.ChangesetView, "error.json", changeset: changeset)
+      end
+    end
+end
