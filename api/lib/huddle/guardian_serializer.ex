438629f3af2a2e720a1023bdaa9ea4d4377f71cf
Roger King <rogr.king@gmail.com>
2017-05-14 14:18:13 -0400

    refactor: huddle to api

---
+++ api/lib/huddle/guardian_serializer.ex
@@ -0,0 +1,13 @@
+defmodule Huddle.GuardianSerializer do
+  @moduledoc false
+  @behavior Guardian.Serializer
+
+  alias Huddle.Repo
+  alias Huddle.Accounts.User
+
+  def for_token(user = %User{}), do: {:ok, "User:#{user.id}"}
+  def for_token(_), do: {:error, "Unknown resource type"}
+
+  def from_token("User:" <> id), do: {:ok, Repo.get(User, String.to_integer(id))}
+  def from_token(_), do: {:error, "Unknown resource type"}
+end
