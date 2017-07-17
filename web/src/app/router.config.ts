3591f8772c2d419ff0edf8e9f11ad6554d682bd9
Roger King <rogr.king@gmail.com>
2017-07-16 20:53:04 -0400

    feat(auth): add auth hook

--- web/src/app/router.config.ts
+++ web/src/app/router.config.ts
@@ -1,12 +1,28 @@
 import * as React from 'react';
 import { pushStateLocationPlugin, UIRouterReact } from '@uirouter/react';
-import { Home , Dashboard } from './containers';
+import { Home, Dashboard } from './containers';
+import { UserService } from './services';
 
-export const states = [ Dashboard, Home ];
+export const states = [Dashboard, Home];
 
 export const plugins = [
   pushStateLocationPlugin
 ];
+
+// Auth Hook
+
+const authHook = {
+  criteria: {
+    to: (state) => state.data && state.data.autheticate
+  },
+  cb: (transition) => {
+    let user = new UserService();
+    let $state = transition.router.stateService;
+    if (user.isAuthenticated()) {
+      return $state.target('home', undefined, { location: false });
+    }
+  }
+};
 
 export const config = (router: UIRouterReact) => {
   // Hook for if route is not registered.
